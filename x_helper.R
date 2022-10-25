d2af6b79237e75fe4c4cbff5b7cceab3f1fa9243
strnda <strnadf@fzp.czu.cz>
2022-10-25 15:19:47 +0200

    addded helper file

---
+++ x_helper.R
@@ -0,0 +1,15 @@
+# install.packages("devtools")
+# install.packages("curl")
+
+library(usethis)
+
+use_git()
+use_github()
+gh_token_help()
+create_github_token()
+gitcreds::gitcreds_set()
+
+curl::curl_download(url = "https://github.com/strnda/env_hydrology/archive/refs/heads/main.zip", 
+                    destfile = "test.zip")
+
+unzip(zipfile = "test.zip")
