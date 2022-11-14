971de80df4820ee0b98cfae787b21e7ef9817801
Filip Strnad <strnadf@fzp.czu.cz>
2022-11-14 11:49:52 +0100

    Update 3_lin_res.R

--- R/3_lin_res.R
+++ R/3_lin_res.R
@@ -23,3 +23,98 @@ plot(x = dta$Q[1:500],
      type = "l")
 lines(x = test[1:500], 
       col = "red")
+
+# install.packages("hydroGOF")
+# install.packages("DEoptim")
+
+library(hydroGOF); library(DEoptim)
+
+ln_model <- function(IN, 
+                     a = .25,  
+                     S0_1 = 100, k_1 = .1,
+                     S0_2 = 15, k_2 = .2,
+                     S0_3 = 50, k_3 = .05) {
+  
+  OUT <- S_1 <- S_2 <- S_3 <-
+    Q_1 <- Q_2 <- Q_3 <- vector(mode = "numeric",
+                                length = length(x = IN))
+  
+  S_1[1] <- S0_1
+  S_2[1] <- S0_2
+  S_3[1] <- S0_3
+  
+  for (i in seq_along(along.with = IN)) {
+    
+    Q_1[i] <- k_1 * S_1[i]
+    S_1[i + 1] <- S_1[i] - Q_1[i] + IN[i] * a
+    
+    Q_2[i] <- k_2 * S_2[i]
+    S_2[i + 1] <- S_2[i] - Q_2[i] + IN[i] * (1 - a)
+    
+    Q_3[i] <- k_3 * S_3[i]
+    S_3[i + 1] <- S_3[i] - Q_3[i] + Q_1[i]
+    
+    OUT[i] <- Q_3[i] + Q_2[i]
+  }
+  
+  sim <- data.frame(IN = IN,
+                    S_1 = S_1[-length(x = IN)],
+                    S_2 = S_2[-length(x = IN)],
+                    S_3 = S_3[-length(x = IN)],
+                    Q_1 = Q_1,
+                    Q_2 = Q_2,
+                    Q_3 = Q_3,
+                    OUT = OUT)
+  sim
+}
+
+test <- ln_model(IN = dta$P)
+
+# plot(x = dta$Q[1:750], 
+#      type = "l")
+# lines(x = test$OUT[1:750], 
+#       col = "red")
+
+dta <- dta[Q >= 0,]
+
+opt_fun <- function(x, dta) {
+  
+  sim <- ln_model(IN = dta$P,
+                  a = x[1],
+                  S0_1 = x[2], k_1 = x[3],
+                  S0_2 = x[4], k_2 = x[5],
+                  S0_3 = x[6], k_3 = x[7])
+  
+  obj <- NSE(sim = sim$OUT, 
+             obs = dta$Q)
+  obj <- (obj - 1) * -1
+  
+  obj
+}
+
+fit <- DEoptim(fn = opt_fun,
+               lower = c(a = 0,
+                         S0_1 = 0, k_1 = 0,
+                         S0_2 = 0, k_2 = 0,
+                         S0_3 = 0, k_3 = 0),
+               upper = c(a = 1,
+                         S0_1 = 500, k_1 = 1,
+                         S0_2 = 500, k_2 = 1,
+                         S0_3 = 500, k_3 = 1), 
+               dta = dta, 
+               control = DEoptim.control(itermax = 50, 
+                                         VTR = 0))
+
+fit$optim$bestmem
+
+sim <- ln_model(IN = dta$P,
+                a = fit$optim$bestmem[1],
+                S0_1 = fit$optim$bestmem[2], k_1 = fit$optim$bestmem[3],
+                S0_2 = fit$optim$bestmem[4], k_2 = fit$optim$bestmem[5],
+                S0_3 = fit$optim$bestmem[6], k_3 = fit$optim$bestmem[7])
+
+
+plot(x = dta$Q[1:750],
+     type = "l")
+lines(x = sim$OUT[1:750],
+      col = "red")
